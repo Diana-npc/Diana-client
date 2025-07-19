e93d66c91614db00ba35e786b0022f383680b499
unknown <dianabalabanova01@gmail.com>
2025-07-19 12:36:00 +0300

    com3

--- src/components/cards/Test.tsx
+++ src/components/cards/Test.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-type TButton = {
-    label: string;
-    onClick: () => void;
-}
+// type TButton = {
+//     label: string;
+//     onClick: () => void;
+// }
 
-export const Button: React.FC<TButton> = ({ label, onClick }) => {
-    const [count, setCount] = useState<number>(0);
-    return (
-        <button onClick={onClick}> {label} </button>
-    )
-}
+// export const Button: React.FC<TButton> = ({ label, onClick }) => {
+//     const [count, setCount] = useState<number>(0);
+//     return (
+//         <button onClick={onClick}> {label} </button>
+//     )
+// }
 
